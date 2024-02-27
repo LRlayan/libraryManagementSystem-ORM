0c741c3dee71c8ab7db8fa32c768e23c08f237a6
LRLayan <rameshlayan4@gmail.com>
2024-02-28 02:40:49 +0530

    start()

--- src/main/java/lk/ijse/Launcher.java
+++ src/main/java/lk/ijse/Launcher.java
@@ -1,4 +1,24 @@
 package lk.ijse;
 
-public class Launcher {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Launcher extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent rootNode = FXMLLoader.load(getClass().getResource("/view/loginForm.fxml"));
+        Scene scene = new Scene(rootNode);
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.show();
+    }
 }
