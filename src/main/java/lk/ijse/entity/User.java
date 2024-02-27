f58182c9a17e6e12f8d61210b09f1d64aa4056e0
LRLayan <rameshlayan4@gmail.com>
2024-02-27 20:28:24 +0530

    user entity

--- src/main/java/lk/ijse/entity/User.java
+++ src/main/java/lk/ijse/entity/User.java
@@ -1,4 +1,22 @@
 package lk.ijse.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+    private String passwords;
 }
