160deb0dceab43fc60663bc5d0364817cebfc22c
LRLayan <rameshlayan4@gmail.com>
2024-02-27 20:27:13 +0530

    admin entity

---
+++ src/main/java/lk/ijse/entity/Admin.java
@@ -0,0 +1,21 @@
+package lk.ijse.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String password;
+}
