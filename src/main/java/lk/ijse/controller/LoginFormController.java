93062b18de90a6dd5d6405ca670ce99233adc4e4
LRLayan <rameshlayan4@gmail.com>
2024-02-28 02:42:52 +0530

    registerOnAction()

--- src/main/java/lk/ijse/controller/LoginFormController.java
+++ src/main/java/lk/ijse/controller/LoginFormController.java
@@ -1,4 +1,20 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import lk.ijse.pageController.PageControl;
+
+import java.io.IOException;
+
 public class LoginFormController {
+
+    @FXML
+    private AnchorPane loginAnchorpane;
+
+    PageControl pageControl = new PageControl();
+
+    public void registerOnAction(ActionEvent event) throws IOException {
+        pageControl.changeOnlyAnchorPane("/view/registerForm.fxml" , loginAnchorpane);
+    }
 }
