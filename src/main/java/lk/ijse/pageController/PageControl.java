41586a2d964ed60c211c31e6bd9092ea38efdc8d
LRLayan <rameshlayan4@gmail.com>
2024-02-28 15:48:45 +0530

    BOFactory

--- src/main/java/lk/ijse/pageController/PageControl.java
+++ src/main/java/lk/ijse/pageController/PageControl.java
@@ -1,7 +1,12 @@
 package lk.ijse.pageController;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -11,5 +16,20 @@ public class PageControl {
         AnchorPane anchorPane = fxmlLoader.load();
         anchorPaneId.getChildren().removeAll();
         anchorPaneId.getChildren().setAll(anchorPane);
+    }
+
+    public void popUpWindow(String fxml) throws IOException {
+        Parent rootNode = FXMLLoader.load(getClass().getResource(fxml));
+        Scene scene = new Scene(rootNode);
+        Stage stage = new Stage();
+        stage.centerOnScreen();
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void closeWindow(JFXButton btnId){
+        Stage stage = (Stage) btnId.getScene().getWindow();
+        stage.close();
     }
 }
