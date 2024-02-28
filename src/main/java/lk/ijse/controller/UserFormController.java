4664b56f0a71061ca2788530c31e475494ced5a7
LRLayan <rameshlayan4@gmail.com>
2024-02-28 23:33:15 +0530

    logoutOnAction()

--- src/main/java/lk/ijse/controller/UserFormController.java
+++ src/main/java/lk/ijse/controller/UserFormController.java
@@ -1,4 +1,22 @@
 package lk.ijse.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import lk.ijse.pageController.PageControl;
+
+import java.io.IOException;
+
 public class UserFormController {
+
+    @FXML
+    private JFXButton btnLogout;
+
+    PageControl pageControl = new PageControl();
+
+    @FXML
+    void logoutOnAction(ActionEvent event) throws IOException {
+        pageControl.closeWindow(btnLogout);
+        pageControl.popUpWindow("/view/loginForm.fxml");
+    }
 }
