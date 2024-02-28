f5bab485621daa9e91f9ddbfdfe28538dd962fa8
LRLayan <rameshlayan4@gmail.com>
2024-02-29 02:36:31 +0530

    Transparent entity

--- src/main/java/lk/ijse/entity/Transaction.java
+++ src/main/java/lk/ijse/entity/Transaction.java
@@ -1,4 +1,24 @@
 package lk.ijse.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String bookTitle;
+    private String nameOfUSer;
+    private String time;
+    private String startDate;
+    private String returnDate;
 }
