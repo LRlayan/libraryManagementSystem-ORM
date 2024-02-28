5edd9e81d67fde356506a96cb83e83cbba6344c1
LRLayan <rameshlayan4@gmail.com>
2024-02-28 23:34:48 +0530

    define label id

---
+++ src/main/java/lk/ijse/controller/HistoryCardFormController.java
@@ -0,0 +1,26 @@
+package lk.ijse.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
+public class HistoryCardFormController {
+
+    @FXML
+    private HBox hBox;
+
+    @FXML
+    private Label bookTitle;
+
+    @FXML
+    private Label startDate;
+
+    @FXML
+    private Label returnDate;
+
+    @FXML
+    private Label time;
+
+    @FXML
+    private Label nameOfUser;
+}
