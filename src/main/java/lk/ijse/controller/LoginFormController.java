e258050698382cd06a943f9667f94ffd4796b6dd
LRLayan <rameshlayan4@gmail.com>
2024-02-28 15:50:13 +0530

    loginOnAction()

--- src/main/java/lk/ijse/controller/LoginFormController.java
+++ src/main/java/lk/ijse/controller/LoginFormController.java
@@ -1,5 +1,6 @@
 package lk.ijse.controller;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
@@ -12,7 +13,16 @@ public class LoginFormController {
     @FXML
     private AnchorPane loginAnchorpane;
 
+    @FXML
+    private JFXButton btnLogin;
+
     PageControl pageControl = new PageControl();
+
+    @FXML
+    void loginOnAction(ActionEvent event) throws IOException {
+        pageControl.popUpWindow("/view/userForm.fxml");
+        pageControl.closeWindow(btnLogin);
+    }
 
     public void registerOnAction(ActionEvent event) throws IOException {
         pageControl.changeOnlyAnchorPane("/view/registerForm.fxml" , loginAnchorpane);
