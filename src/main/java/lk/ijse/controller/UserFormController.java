f2bd55ae5ae5f308edd6cb3d16e7ae0920304b4c
LRLayan <rameshlayan4@gmail.com>
2024-02-28 23:50:28 +0530

    initialize()

--- src/main/java/lk/ijse/controller/UserFormController.java
+++ src/main/java/lk/ijse/controller/UserFormController.java
@@ -3,14 +3,21 @@ package lk.ijse.controller;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
 import lk.ijse.pageController.PageControl;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class UserFormController {
+public class UserFormController implements Initializable {
 
     @FXML
     private JFXButton btnLogout;
+
+    @FXML
+    private VBox vBox;
 
     PageControl pageControl = new PageControl();
 
@@ -18,5 +25,10 @@ public class UserFormController {
     void logoutOnAction(ActionEvent event) throws IOException {
         pageControl.closeWindow(btnLogout);
         pageControl.popUpWindow("/view/loginForm.fxml");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
