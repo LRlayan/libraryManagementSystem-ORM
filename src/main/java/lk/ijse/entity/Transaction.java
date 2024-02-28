0140c165a2649f2240c9b6c68b50f5e0cf0dab43
LRLayan <rameshlayan4@gmail.com>
2024-02-28 23:50:51 +0530

    Transaction entity

--- src/main/java/lk/ijse/entity/Transaction.java
+++ src/main/java/lk/ijse/entity/Transaction.java
@@ -1,26 +1,4 @@
 package lk.ijse.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
-@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Transaction {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-    private String bookTitle;
-    private String buyDate;
-    private String returnDate;
-    private LocalDateTime time;
 }
