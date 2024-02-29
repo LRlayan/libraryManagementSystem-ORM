aae2b0d7510f02219c82b7c78b3581688345e479
LRLayan <rameshlayan4@gmail.com>
2024-03-01 02:03:24 +0530

    DAOFactory implement

--- src/main/java/lk/ijse/dao/DAOFactory.java
+++ src/main/java/lk/ijse/dao/DAOFactory.java
@@ -1,4 +1,27 @@
 package lk.ijse.dao;
 
+import lk.ijse.dao.custom.impl.TransactionDAOImpl;
+import lk.ijse.dao.custom.impl.UserDAOImpl;
+
 public class DAOFactory {
+    private static DAOFactory daoFactory;
+
+    private DAOFactory(){}
+
+    public static DAOFactory getDaoFactory(){
+        return (daoFactory == null) ? daoFactory = new DAOFactory() : daoFactory;
+    }
+    public enum DAOTypes{
+        USER,TRANSACTION,BRANCHES,ADMIN,BOOK
+    }
+    public SuperDAO DAOTypes(DAOTypes daoTypes){
+        switch (daoTypes){
+            case USER:
+                return new UserDAOImpl();
+            case TRANSACTION:
+                return new TransactionDAOImpl();
+            default:
+                return null;
+        }
+    }
 }
