23e9576ffb6f3fedf1183d1a981fc54720933f16
LRLayan <rameshlayan4@gmail.com>
2024-03-01 02:05:32 +0530

    save() , update() , delete()

--- src/main/java/lk/ijse/dao/custom/impl/UserDAOImpl.java
+++ src/main/java/lk/ijse/dao/custom/impl/UserDAOImpl.java
@@ -1,6 +1,31 @@
 package lk.ijse.dao.custom.impl;
 
+import lk.ijse.config.FactoryConfiguration;
 import lk.ijse.dao.custom.UserDAO;
+import lk.ijse.entity.User;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public class UserDAOImpl implements UserDAO {
+    @Override
+    public boolean save(User user) {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+        session.save(user);
+
+        transaction.commit();
+        session.close();
+        return true;
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
 }
