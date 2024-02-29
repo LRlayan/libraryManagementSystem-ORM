7b1cb2d81332d253e6e8f2a7a30c452aa580ed03
LRLayan <rameshlayan4@gmail.com>
2024-03-01 02:03:46 +0530

    RegisterDTO

---
+++ src/main/java/lk/ijse/dto/RegisterDTO.java
@@ -0,0 +1,15 @@
+package lk.ijse.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterDTO {
+    private long id;
+    private String name;
+    private String email;
+    private String passwords;
+}
