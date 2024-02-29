5408c52621aee605a64b98a7b980e52ed5c9c623
LRLayan <rameshlayan4@gmail.com>
2024-03-01 02:04:15 +0530

    RegisterFormBO

--- src/main/java/lk/ijse/bo/custom/RegisterFormBO.java
+++ src/main/java/lk/ijse/bo/custom/RegisterFormBO.java
@@ -1,6 +1,8 @@
 package lk.ijse.bo.custom;
 
 import lk.ijse.bo.SuperBO;
+import lk.ijse.dto.RegisterDTO;
 
 public interface RegisterFormBO extends SuperBO {
+    boolean saveUser(RegisterDTO registerDTO);
 }
