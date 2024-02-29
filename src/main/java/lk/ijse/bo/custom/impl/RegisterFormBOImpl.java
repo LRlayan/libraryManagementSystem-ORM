bc4b71bc09f6ecd8442a1fb00ba3b9e52d770443
LRLayan <rameshlayan4@gmail.com>
2024-03-01 02:04:40 +0530

    saveUser()

--- src/main/java/lk/ijse/bo/custom/impl/RegisterFormBOImpl.java
+++ src/main/java/lk/ijse/bo/custom/impl/RegisterFormBOImpl.java
@@ -1,6 +1,17 @@
 package lk.ijse.bo.custom.impl;
 
 import lk.ijse.bo.custom.RegisterFormBO;
+import lk.ijse.dao.DAOFactory;
+import lk.ijse.dao.custom.UserDAO;
+import lk.ijse.dto.RegisterDTO;
+import lk.ijse.entity.User;
 
 public class RegisterFormBOImpl implements RegisterFormBO {
+
+    UserDAO userDAO = (UserDAO) DAOFactory.getDaoFactory().DAOTypes(DAOFactory.DAOTypes.USER);
+
+    @Override
+    public boolean saveUser(RegisterDTO registerDTO) {
+        return userDAO.save(new User(registerDTO.getId(),registerDTO.getName(),registerDTO.getEmail(),registerDTO.getPasswords()));
+    }
 }
