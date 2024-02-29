a8797f99a82f45030fda354000a2c18f0fbbc1e1
LRLayan <rameshlayan4@gmail.com>
2024-03-01 02:03:07 +0530

    BOFactory implement

--- src/main/java/lk/ijse/bo/BOFactory.java
+++ src/main/java/lk/ijse/bo/BOFactory.java
@@ -1,4 +1,35 @@
 package lk.ijse.bo;
 
+import lk.ijse.bo.custom.impl.*;
+
 public class BOFactory {
+
+    private static BOFactory boFactory;
+
+    private BOFactory(){}
+
+    public static BOFactory getBoFactory(){
+        return (boFactory == null) ? boFactory = new BOFactory() : boFactory;
+    }
+
+    public enum BOTypes{
+        USER,SETTING,REGISTER,LOGIN,HISTORY
+    }
+
+    public SuperBO BOTypes(BOTypes boTypes){
+        switch (boTypes){
+            case USER:
+                return new UserFormBOImpl();
+            case SETTING:
+                return new SettingFormBOImpl();
+            case REGISTER:
+                return new RegisterFormBOImpl();
+            case LOGIN:
+                return new LoginFormBOImpl();
+            case HISTORY:
+                return new HistoryCardFormBOImpl();
+            default:
+                return null;
+        }
+    }
 }
