6c5cde078c261e32eceab5eb5985834b3bb13317
LRLayan <rameshlayan4@gmail.com>
2024-02-29 13:48:14 +0530

    Transaction addAnnotatedClass

--- src/main/java/lk/ijse/controller/LoginFormController.java
+++ src/main/java/lk/ijse/controller/LoginFormController.java
@@ -4,7 +4,10 @@ import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
+import lk.ijse.config.FactoryConfiguration;
 import lk.ijse.pageController.PageControl;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.io.IOException;
 
@@ -22,6 +25,12 @@ public class LoginFormController {
     void loginOnAction(ActionEvent event) throws IOException {
         pageControl.popUpWindow("/view/userForm.fxml");
         pageControl.closeWindow(btnLogin);
+
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+        transaction.commit();
+        session.close();
     }
 
     public void registerOnAction(ActionEvent event) throws IOException {
