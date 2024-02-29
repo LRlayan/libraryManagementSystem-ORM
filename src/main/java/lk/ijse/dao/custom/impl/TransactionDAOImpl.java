5217e814a12f183ec8f0c0e665bd0d7f4fbcdf0d
LRLayan <rameshlayan4@gmail.com>
2024-03-01 02:05:21 +0530

    save() , update() , delete()

--- src/main/java/lk/ijse/dao/custom/impl/TransactionDAOImpl.java
+++ src/main/java/lk/ijse/dao/custom/impl/TransactionDAOImpl.java
@@ -1,6 +1,22 @@
 package lk.ijse.dao.custom.impl;
 
 import lk.ijse.dao.custom.TransactionDAO;
+import lk.ijse.entity.User;
 
 public class TransactionDAOImpl implements TransactionDAO {
+    @Override
+    public boolean save(User user) {
+
+        return false;
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
 }
