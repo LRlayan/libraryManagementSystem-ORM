6c4e759c71524aa7c24aa524a9c21bec4da92798
LRLayan <rameshlayan4@gmail.com>
2024-03-03 00:13:10 +0530

    saveChangesOnAction() , define fxId , closeButtonOnAction()

--- src/main/java/lk/ijse/controller/SettingFormController.java
+++ src/main/java/lk/ijse/controller/SettingFormController.java
@@ -4,6 +4,15 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import lk.ijse.bo.BOFactory;
+import lk.ijse.bo.custom.RegisterFormBO;
+import lk.ijse.bo.custom.SettingFormBO;
+import lk.ijse.dto.RegisterDTO;
+import lk.ijse.pageController.PageControl;
+
+import java.util.List;
 
 public class SettingFormController {
     @FXML
@@ -28,12 +37,76 @@ public class SettingFormController {
     private JFXButton btnClose;
 
     @FXML
-    void closeButtonOnAction(ActionEvent event) {
+    private Label lblCurrentUsername;
 
+    @FXML
+    private Label lblNewUsername;
+
+    @FXML
+    private Label lblConfirmUsername;
+
+    @FXML
+    private Label lblCurrentPassword;
+
+    @FXML
+    private Label lblNewPassword;
+
+    @FXML
+    private Label lblConfirmPassword;
+
+    PageControl pageControl = new PageControl();
+
+    RegisterFormBO registerFormBO = (RegisterFormBO) BOFactory.getBoFactory().BOTypes(BOFactory.BOTypes.REGISTER);
+    SettingFormBO settingFormBO = (SettingFormBO) BOFactory.getBoFactory().BOTypes(BOFactory.BOTypes.SETTING);
+    @FXML
+    void closeButtonOnAction(ActionEvent event) {
+        pageControl.closeWindow(btnClose);
     }
 
     @FXML
     void saveChangesOnAction(ActionEvent event) {
 
+
+        String currentUsername = txtCurrentUsername.getText();
+        String newUsername = txtNewUsername.getText();
+        String confirmUsername = txtConfirmUsername.getText();
+
+        String currentPassword = txtCurrentPassword.getText();
+        String newPassword = txtNewPassword.getText();
+        String confirmPassword = txtConfirmPassword.getText();
+
+        if (!currentUsername.isEmpty() && !newUsername.isEmpty() && !confirmUsername.isEmpty()){
+
+             boolean isChecked = checkingUsername(confirmUsername,newUsername,confirmUsername);
+             if (isChecked){
+                 var userDTO = new RegisterDTO();
+                 userDTO.setName(confirmUsername);
+                 boolean isUpdated = settingFormBO.updateCredential(userDTO);
+
+                 if (isUpdated){
+                     new Alert(Alert.AlertType.INFORMATION,"Updated Successfully").show();
+                 }
+             }
+        }else if (!currentPassword.isEmpty() && !newPassword.isEmpty() && !confirmPassword.isEmpty()){
+
+        }
+    }
+
+    private boolean checkingUsername(String currentUsername , String newUsername , String confirmUsername) {
+        boolean isChecked = false;
+        List<RegisterDTO> registerUsers = registerFormBO.getAll();
+
+        for (int i = 0; i < registerUsers.size(); i++){
+            if (currentUsername.equals(registerUsers.get(i).getName())){
+                if (newUsername.equals(confirmUsername)){
+                   isChecked = true;
+                }else {
+                    lblNewUsername.setText("incorrect username.please reEnter new username.");
+                }
+            }else {
+                lblCurrentUsername.setText("Please input correct username");
+            }
+        }
+        return isChecked;
     }
 }
