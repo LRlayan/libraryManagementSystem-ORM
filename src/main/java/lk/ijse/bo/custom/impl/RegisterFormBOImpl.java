56d4aed92aaacbcdfdf14517c65320c6438c103b
LRLayan <rameshlayan4@gmail.com>
2024-03-03 00:11:26 +0530

    getALl()

--- src/main/java/lk/ijse/bo/custom/impl/RegisterFormBOImpl.java
+++ src/main/java/lk/ijse/bo/custom/impl/RegisterFormBOImpl.java
@@ -6,6 +6,9 @@ import lk.ijse.dao.custom.UserDAO;
 import lk.ijse.dto.RegisterDTO;
 import lk.ijse.entity.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RegisterFormBOImpl implements RegisterFormBO {
 
     UserDAO userDAO = (UserDAO) DAOFactory.getDaoFactory().DAOTypes(DAOFactory.DAOTypes.USER);
@@ -13,5 +16,16 @@ public class RegisterFormBOImpl implements RegisterFormBO {
     @Override
     public boolean saveUser(RegisterDTO registerDTO) {
         return userDAO.save(new User(registerDTO.getId(),registerDTO.getName(),registerDTO.getEmail(),registerDTO.getPasswords()));
+    }
+
+    @Override
+    public List<RegisterDTO> getAll() {
+        List<User> getAll = userDAO.getAll();
+        List<RegisterDTO> registerDTO = new ArrayList<>();
+
+        for (User register : getAll){
+            registerDTO.add(new RegisterDTO(register.getId(), register.getName(), register.getEmail(),register.getPasswords()));
+        }
+        return registerDTO;
     }
 }
