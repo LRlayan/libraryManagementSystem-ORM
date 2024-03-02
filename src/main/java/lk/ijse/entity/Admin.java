46e9312d1761167b29d8ab40ec8fd5e7a99b6695
LRLayan <rameshlayan4@gmail.com>
2024-03-03 00:09:31 +0530

    admin entity

--- src/main/java/lk/ijse/entity/Admin.java
+++ src/main/java/lk/ijse/entity/Admin.java
@@ -16,6 +16,6 @@ public class Admin {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private String name;
+    private String username;
     private String password;
 }
