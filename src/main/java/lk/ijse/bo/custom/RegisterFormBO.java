98ffad79895a191d4c720bc660c444df98954e5f
LRLayan <rameshlayan4@gmail.com>
2024-03-03 00:11:13 +0530

    getALl()

--- src/main/java/lk/ijse/bo/custom/RegisterFormBO.java
+++ src/main/java/lk/ijse/bo/custom/RegisterFormBO.java
@@ -3,6 +3,9 @@ package lk.ijse.bo.custom;
 import lk.ijse.bo.SuperBO;
 import lk.ijse.dto.RegisterDTO;
 
+import java.util.List;
+
 public interface RegisterFormBO extends SuperBO {
     boolean saveUser(RegisterDTO registerDTO);
+    List<RegisterDTO> getAll();
 }
