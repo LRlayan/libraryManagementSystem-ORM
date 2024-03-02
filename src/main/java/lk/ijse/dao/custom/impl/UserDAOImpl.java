cdfdab8317f3060f8b6dbe5966633c9eea457faa
LRLayan <rameshlayan4@gmail.com>
2024-03-03 00:15:45 +0530

    update() , getAll()

--- src/main/java/lk/ijse/dao/custom/impl/UserDAOImpl.java
+++ src/main/java/lk/ijse/dao/custom/impl/UserDAOImpl.java
@@ -6,6 +6,8 @@ import lk.ijse.entity.User;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import java.util.List;
+
 public class UserDAOImpl implements UserDAO {
     @Override
     public boolean save(User user) {
@@ -20,12 +22,25 @@ public class UserDAOImpl implements UserDAO {
     }
 
     @Override
-    public void update() {
+    public boolean update(User user) {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
 
+        //session.createQuery()
+
+        transaction.commit();
+        session.close();
+        return false;
     }
 
     @Override
     public void delete() {
 
+    }
+
+    @Override
+    public List<User> getAll() {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        return session.createQuery("FROM User ").list();
     }
 }
