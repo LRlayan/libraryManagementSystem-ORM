b7df154900fae196194e7230579726789b0d1c37
LRLayan <rameshlayan4@gmail.com>
2024-03-03 00:13:30 +0530

    relationship

--- src/main/java/lk/ijse/entity/Transaction.java
+++ src/main/java/lk/ijse/entity/Transaction.java
@@ -1,9 +1,6 @@
 package lk.ijse.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,4 +18,7 @@ public class Transaction {
     private String time;
     private String startDate;
     private String returnDate;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
