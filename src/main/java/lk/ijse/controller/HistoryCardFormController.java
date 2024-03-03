5a4a4bd6f3f7ad14826fa77c102faf09b33800e2
LRLayan <rameshlayan4@gmail.com>
2024-03-04 00:02:55 +0530

    remove code

--- src/main/java/lk/ijse/controller/HistoryCardFormController.java
+++ src/main/java/lk/ijse/controller/HistoryCardFormController.java
@@ -1,45 +1,13 @@
 package lk.ijse.controller;
 
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-import lk.ijse.entity.Transaction;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HistoryCardFormController implements Initializable {
 
-    @FXML
-    private HBox hBox;
-
-    @FXML
-    private Label bookTitle;
-
-    @FXML
-    private Label startDate;
-
-    @FXML
-    private Label returnDate;
-
-    @FXML
-    private Label time;
-
-    @FXML
-    private Label nameOfUser;
-
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
-    }
-
-    public void setData(Transaction transaction){
-        bookTitle.setText(transaction.getBookTitle());
-        startDate.setText(transaction.getStartDate());
-        returnDate.setText(transaction.getReturnDate());
-        time.setText(transaction.getTime());
-        nameOfUser.setText(transaction.getNameOfUSer());
     }
 }
