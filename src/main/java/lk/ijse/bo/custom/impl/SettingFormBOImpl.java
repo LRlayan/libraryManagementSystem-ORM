6f71d0e16bd18a8d3a915a0ddb1cdc2a23c700d5
LRLayan <rameshlayan4@gmail.com>
2024-03-04 00:02:03 +0530

    object creational bug fix

--- src/main/java/lk/ijse/bo/custom/impl/SettingFormBOImpl.java
+++ src/main/java/lk/ijse/bo/custom/impl/SettingFormBOImpl.java
@@ -2,13 +2,14 @@ package lk.ijse.bo.custom.impl;
 
 import lk.ijse.bo.BOFactory;
 import lk.ijse.bo.custom.SettingFormBO;
+import lk.ijse.dao.DAOFactory;
 import lk.ijse.dao.custom.UserDAO;
 import lk.ijse.dto.RegisterDTO;
 import lk.ijse.entity.User;
 
 public class SettingFormBOImpl implements SettingFormBO {
 
-    UserDAO userDAO = (UserDAO) BOFactory.getBoFactory().BOTypes(BOFactory.BOTypes.USER);
+    UserDAO userDAO = (UserDAO) DAOFactory.getDaoFactory().DAOTypes(DAOFactory.DAOTypes.USER);
 
     @Override
     public boolean updateCredential(RegisterDTO userDTO) {
