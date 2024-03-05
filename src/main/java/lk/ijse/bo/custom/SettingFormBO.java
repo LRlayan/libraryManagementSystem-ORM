8d473ad0c5c6023603a66c1192ee1242de82ded4
LRLayan <rameshlayan4@gmail.com>
2024-03-06 01:14:36 +0530

    List<RegisterDTO> getAllUser();

--- src/main/java/lk/ijse/bo/custom/SettingFormBO.java
+++ src/main/java/lk/ijse/bo/custom/SettingFormBO.java
@@ -3,6 +3,9 @@ package lk.ijse.bo.custom;
 import lk.ijse.bo.SuperBO;
 import lk.ijse.dto.RegisterDTO;
 
+import java.util.List;
+
 public interface SettingFormBO extends SuperBO {
     boolean updateCredential(RegisterDTO userDTO);
+    List<RegisterDTO> getAllUser();
 }
