908237a1244c35c9a6165db1d14a8bcebb02fb62
LRLayan <rameshlayan4@gmail.com>
2024-03-06 00:36:12 +0530

    List<BookDTO> getAllBooks();

--- src/main/java/lk/ijse/bo/custom/UserBO.java
+++ src/main/java/lk/ijse/bo/custom/UserBO.java
@@ -1,6 +1,10 @@
 package lk.ijse.bo.custom;
 
 import lk.ijse.bo.SuperBO;
+import lk.ijse.dto.BookDTO;
+
+import java.util.List;
 
 public interface UserBO extends SuperBO {
+    List<BookDTO> getAllBooks();
 }
