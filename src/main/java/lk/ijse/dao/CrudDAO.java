63078a88bd8b9ead1cabfc3167c56bbb47f99656
LRLayan <rameshlayan4@gmail.com>
2024-03-06 00:47:32 +0530

    use type parameter

--- src/main/java/lk/ijse/dao/CrudDAO.java
+++ src/main/java/lk/ijse/dao/CrudDAO.java
@@ -1,13 +1,11 @@
 package lk.ijse.dao;
 
-import lk.ijse.entity.Books;
 import lk.ijse.entity.User;
-
 import java.util.List;
 
 public interface CrudDAO<T> extends SuperDAO {
    boolean save(User user);
    boolean update(User user);
    void delete();
-   List<Books> getAll();
+   List<T> getAll();
 }
