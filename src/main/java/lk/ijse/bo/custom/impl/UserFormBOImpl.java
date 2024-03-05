d3dddb1796762cb0af41ac4fb21feb154c46e727
LRLayan <rameshlayan4@gmail.com>
2024-03-06 00:37:07 +0530

    List<BookDTO> getAllBooks();

--- src/main/java/lk/ijse/bo/custom/impl/UserFormBOImpl.java
+++ src/main/java/lk/ijse/bo/custom/impl/UserFormBOImpl.java
@@ -1,6 +1,27 @@
 package lk.ijse.bo.custom.impl;
 
+import lk.ijse.bo.BOFactory;
 import lk.ijse.bo.custom.UserBO;
+import lk.ijse.dao.DAOFactory;
+import lk.ijse.dao.custom.BookDAO;
+import lk.ijse.dto.BookDTO;
+import lk.ijse.entity.Books;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserFormBOImpl implements UserBO {
+
+    BookDAO bookDAO = (BookDAO) DAOFactory.getDaoFactory().DAOTypes(DAOFactory.DAOTypes.BOOK);
+
+    @Override
+    public List<BookDTO> getAllBooks() {
+        List<Books> bookList = bookDAO.getAll();
+        List<BookDTO> bookDTOS = new ArrayList<>();
+
+        for (Books book : bookList){
+            bookDTOS.add(new BookDTO(book.getId(),book.getTitle(),book.getAuthor(),book.getGenre(),book.getAvailabilityStatus()));
+        }
+        return bookDTOS;
+    }
 }
