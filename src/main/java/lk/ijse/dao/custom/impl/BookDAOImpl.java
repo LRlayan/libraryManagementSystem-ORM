bdd0af9185d8bdcff9ecfa5508e6b23cc7e5baac
LRLayan <rameshlayan4@gmail.com>
2024-03-06 00:31:28 +0530

    implement method

--- src/main/java/lk/ijse/dao/custom/impl/BookDAOImpl.java
+++ src/main/java/lk/ijse/dao/custom/impl/BookDAOImpl.java
@@ -1,4 +1,34 @@
 package lk.ijse.dao.custom.impl;
 
-public class BookDAOImpl {
+import lk.ijse.config.FactoryConfiguration;
+import lk.ijse.dao.custom.BookDAO;
+import lk.ijse.entity.Books;
+import lk.ijse.entity.User;
+import org.hibernate.Session;
+
+import java.util.List;
+
+public class BookDAOImpl implements BookDAO {
+
+    @Override
+    public boolean save(User user) {
+        return false;
+    }
+
+    @Override
+    public boolean update(User user) {
+        return false;
+    }
+
+    @Override
+    public void delete() {
+
+    }
+
+    @Override
+    public List<Books> getAll() {
+
+        Session session = FactoryConfiguration.getInstance().getSession();
+        return session.createQuery("FROM Books ").list();
+    }
 }
