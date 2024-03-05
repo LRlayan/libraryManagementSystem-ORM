592f25a25198c03f190d79a6d68980062711d6ba
LRLayan <rameshlayan4@gmail.com>
2024-03-06 00:31:47 +0530

    implement bookDTO

---
+++ src/main/java/lk/ijse/dto/BookDTO.java
@@ -0,0 +1,16 @@
+package lk.ijse.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookDTO {
+    private long id;
+    private String title;
+    private String author;
+    private String genre;
+    private String availabilityStatus;
+}
