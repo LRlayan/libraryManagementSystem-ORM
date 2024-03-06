7847d933a12a0f6c698d83fa8f390c8202006aec
LRLayan <rameshlayan4@gmail.com>
2024-03-06 19:10:22 +0530

    BranchDTO

---
+++ src/main/java/lk/ijse/dto/BranchDTO.java
@@ -0,0 +1,13 @@
+package lk.ijse.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BranchDTO {
+    private long id;
+    private String branchName;
+}
