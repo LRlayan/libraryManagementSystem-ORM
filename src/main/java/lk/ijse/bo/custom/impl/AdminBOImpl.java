af952330a1b222e12a0230d8fd559620020378d2
LRLayan <rameshlayan4@gmail.com>
2024-03-06 18:48:45 +0530

    implement methods

--- src/main/java/lk/ijse/bo/custom/impl/AdminBOImpl.java
+++ src/main/java/lk/ijse/bo/custom/impl/AdminBOImpl.java
@@ -1,6 +1,34 @@
 package lk.ijse.bo.custom.impl;
 
 import lk.ijse.bo.custom.AdminBO;
+import lk.ijse.dao.DAOFactory;
+import lk.ijse.dao.custom.AdminDAO;
+import lk.ijse.dto.AdminDTO;
+import lk.ijse.entity.Admin;
+
+import java.util.List;
 
 public class AdminBOImpl implements AdminBO {
+
+    AdminDAO adminDAO = (AdminDAO) DAOFactory.getDaoFactory().DAOTypes(DAOFactory.DAOTypes.ADMIN);
+
+    @Override
+    public boolean saveAdmin(AdminDTO adminDTO) {
+       return adminDAO.save(new Admin(adminDTO.getId(),adminDTO.getUsername(),adminDTO.getPassword()));
+    }
+
+    @Override
+    public boolean updateAdmin(AdminDTO adminDTO) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteAdmin() {
+        return false;
+    }
+
+    @Override
+    public List<Admin> getAllAdmin() {
+        return null;
+    }
 }
