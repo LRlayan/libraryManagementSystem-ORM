2c1259b46c36b5e4b3a3733a9545f0457d9dcfbf
LRLayan <rameshlayan4@gmail.com>
2024-03-06 19:13:04 +0530

    createAdmin()

--- src/main/java/lk/ijse/controller/LoginFormController.java
+++ src/main/java/lk/ijse/controller/LoginFormController.java
@@ -3,15 +3,22 @@ package lk.ijse.controller;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
+import lk.ijse.bo.BOFactory;
+import lk.ijse.bo.custom.AdminBO;
 import lk.ijse.config.FactoryConfiguration;
+import lk.ijse.dto.AdminDTO;
 import lk.ijse.pageController.PageControl;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class LoginFormController {
+public class LoginFormController implements Initializable {
 
     @FXML
     private AnchorPane loginAnchorpane;
@@ -23,6 +30,8 @@ public class LoginFormController {
     private JFXButton btnClose;
 
     PageControl pageControl = new PageControl();
+
+    AdminBO adminBO = (AdminBO) BOFactory.getBoFactory().BOTypes(BOFactory.BOTypes.ADMIN);
 
     @FXML
     void loginOnAction(ActionEvent event) throws IOException {
@@ -43,5 +52,29 @@ public class LoginFormController {
     @FXML
     void closeButtonOnAction(ActionEvent event) {
         pageControl.closeWindow(btnClose);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        createAdmin();
+    }
+
+    private void createAdmin() {
+        String username = "admin";
+        String password = "1234";
+        int branchesId = 1;
+
+        try{
+            var adminDTO = new AdminDTO(0,username,password);
+            boolean isSaved = adminBO.saveAdmin(adminDTO);
+
+            if (isSaved){
+                new Alert(Alert.AlertType.INFORMATION,"saved admin").show();
+            }else {
+                new Alert(Alert.AlertType.ERROR,"Not saved").show();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
