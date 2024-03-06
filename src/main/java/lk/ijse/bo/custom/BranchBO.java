07ce9b2bbed0602f9907fcf2da15f1d1a6a99769
LRLayan <rameshlayan4@gmail.com>
2024-03-06 18:56:08 +0530

    create method

--- src/main/java/lk/ijse/bo/custom/BranchBO.java
+++ src/main/java/lk/ijse/bo/custom/BranchBO.java
@@ -1,6 +1,13 @@
 package lk.ijse.bo.custom;
 
 import lk.ijse.bo.SuperBO;
+import lk.ijse.dto.BranchDTO;
+
+import java.util.List;
 
 public interface BranchBO extends SuperBO {
+    boolean saveBranches(BranchDTO entity);
+    boolean updateBranches(BranchDTO entity);
+    void deleteBranches();
+    List<BranchDTO> getAllBranches();
 }
