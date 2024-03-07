4bc46e81bf506eb29325e6979a5b14b3419c074d
LRLayan <rameshlayan4@gmail.com>
2024-03-07 20:15:50 +0530

    currentBranches()

--- src/main/java/lk/ijse/Launcher.java
+++ src/main/java/lk/ijse/Launcher.java
@@ -4,16 +4,36 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import lk.ijse.bo.BOFactory;
+import lk.ijse.bo.custom.BranchBO;
+import lk.ijse.dto.BranchDTO;
 
 public class Launcher extends Application {
+     BranchBO branchBO = (BranchBO) BOFactory.getBoFactory().BOTypes(BOFactory.BOTypes.BRANCH);
+
     public static void main(String[] args) {
         launch(args);
     }
 
+    private void currentBranches() {
+        String branchIdNumber = "001";
+        String branchName = "Beruwala";
+
+        try{
+            var branch = new BranchDTO(0,branchIdNumber,branchName);
+            boolean isSaved = branchBO.saveBranches(branch);
+
+        }catch (Exception e){
+            new Alert(Alert.AlertType.ERROR,"not saved branches");
+        }
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
+        currentBranches();
         Parent rootNode = FXMLLoader.load(getClass().getResource("/view/loginForm.fxml"));
         Scene scene = new Scene(rootNode);
         stage.setScene(scene);
