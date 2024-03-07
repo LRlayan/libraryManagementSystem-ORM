7eebba98310a60eb6d924a7768443f04f448d97f
LRLayan <rameshlayan4@gmail.com>
2024-03-07 22:13:56 +0530

    get All Admin

--- src/main/java/lk/ijse/bo/custom/LoginFormBO.java
+++ src/main/java/lk/ijse/bo/custom/LoginFormBO.java
@@ -1,6 +1,10 @@
 package lk.ijse.bo.custom;
 
 import lk.ijse.bo.SuperBO;
+import lk.ijse.dto.AdminDTO;
+
+import java.util.List;
 
 public interface LoginFormBO extends SuperBO {
+    List<AdminDTO> getAllAdmin();
 }
