282342ae3bc7c3204b36b0e5ed839605da367e2d
LRLayan <rameshlayan4@gmail.com>
2024-03-07 15:59:11 +0530

    getAllBranches()

--- src/main/java/lk/ijse/bo/custom/RegisterFormBO.java
+++ src/main/java/lk/ijse/bo/custom/RegisterFormBO.java
@@ -1,6 +1,7 @@
 package lk.ijse.bo.custom;
 
 import lk.ijse.bo.SuperBO;
+import lk.ijse.dto.BranchDTO;
 import lk.ijse.dto.RegisterDTO;
 
 import java.util.List;
@@ -8,4 +9,5 @@ import java.util.List;
 public interface RegisterFormBO extends SuperBO {
     boolean saveUser(RegisterDTO registerDTO);
     List<RegisterDTO> getAllUser();
+    List<BranchDTO> getAllBranches();
 }
