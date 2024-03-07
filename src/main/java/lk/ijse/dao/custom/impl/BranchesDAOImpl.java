c66bb8bab6f8a6c795ff28efe6e6feccece6ec0b
LRLayan <rameshlayan4@gmail.com>
2024-03-07 15:57:03 +0530

    save()

--- src/main/java/lk/ijse/dao/custom/impl/BranchesDAOImpl.java
+++ src/main/java/lk/ijse/dao/custom/impl/BranchesDAOImpl.java
@@ -4,13 +4,23 @@ import lk.ijse.config.FactoryConfiguration;
 import lk.ijse.dao.custom.BranchesDAO;
 import lk.ijse.entity.Branches;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
 public class BranchesDAOImpl implements BranchesDAO {
     @Override
     public boolean save(Branches branches) {
-        return false;
+       Session session = FactoryConfiguration.getInstance().getSession();
+       Transaction transaction = session.beginTransaction();
+
+       AdminDAOImpl.branches = branches;
+       UserDAOImpl.branches = branches;
+       session.save(branches);
+
+       transaction.commit();
+       session.close();
+       return true;
     }
 
     @Override
