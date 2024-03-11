e7c5c2bd6765a979557bde0cf4c25e2ad8dbe4b4
LRLayan <rameshlayan4@gmail.com>
2024-03-11 21:46:17 +0530

    getAllUsers()

--- src/main/java/lk/ijse/bo/custom/impl/UserFormBOImpl.java
+++ src/main/java/lk/ijse/bo/custom/impl/UserFormBOImpl.java
@@ -4,8 +4,11 @@ import lk.ijse.bo.BOFactory;
 import lk.ijse.bo.custom.UserBO;
 import lk.ijse.dao.DAOFactory;
 import lk.ijse.dao.custom.BookDAO;
+import lk.ijse.dao.custom.UserDAO;
 import lk.ijse.dto.BookDTO;
+import lk.ijse.dto.UserDTO;
 import lk.ijse.entity.Books;
+import lk.ijse.entity.User;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +16,7 @@ import java.util.List;
 public class UserFormBOImpl implements UserBO {
 
     BookDAO bookDAO = (BookDAO) DAOFactory.getDaoFactory().DAOTypes(DAOFactory.DAOTypes.BOOK);
+    UserDAO userDAO = (UserDAO) DAOFactory.getDaoFactory().DAOTypes(DAOFactory.DAOTypes.USER);
 
     @Override
     public List<BookDTO> getAllBooks() {
@@ -23,5 +27,16 @@ public class UserFormBOImpl implements UserBO {
             bookDTOS.add(new BookDTO(book.getId(),book.getTitle(),book.getAuthor(),book.getGenre(),book.getAvailabilityStatus()));
         }
         return bookDTOS;
+    }
+
+    @Override
+    public List<UserDTO> getAllUsers() {
+        List<User> userList = userDAO.getAll();
+        List<UserDTO> userDTOS = new ArrayList<>();
+
+        for (User user : userList){
+            userDTOS.add(new UserDTO(user.getId(),user.getName(),user.getEmail(),user.getPasswords(),user.getBranchName()));
+        }
+        return userDTOS;
     }
 }
