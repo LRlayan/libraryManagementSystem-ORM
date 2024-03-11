a596d335b0bc8b31435ec885c040490d0f15bb10
LRLayan <rameshlayan4@gmail.com>
2024-03-11 21:45:00 +0530

    BookTM

---
+++ src/main/java/lk/ijse/dto/tm/BookTM.java
@@ -0,0 +1,16 @@
+package lk.ijse.dto.tm;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookTM {
+    private long id;
+    private String title;
+    private String author;
+    private String genre;
+    private String availabilityStatus;
+}
