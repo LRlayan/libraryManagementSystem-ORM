3152a0934f8a1f0104411df73b744d9d405e6bc3
LRLayan <rameshlayan4@gmail.com>
2024-03-11 21:45:59 +0530

    UserDTO

---
+++ src/main/java/lk/ijse/dto/UserDTO.java
@@ -0,0 +1,16 @@
+package lk.ijse.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private long id;
+    private String name;
+    private String email;
+    private String password;
+    private String branch;
+}
