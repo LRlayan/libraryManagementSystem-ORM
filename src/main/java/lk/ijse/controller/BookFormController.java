f84ad251ad64e500d5b511404cfa091bdcd413df
LRLayan <rameshlayan4@gmail.com>
2024-03-11 21:44:52 +0530

    loadAllBooks() , setCellValueFactory()

--- src/main/java/lk/ijse/controller/BookFormController.java
+++ src/main/java/lk/ijse/controller/BookFormController.java
@@ -1,21 +1,36 @@
 package lk.ijse.controller;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import lk.ijse.bo.BOFactory;
+import lk.ijse.bo.custom.BookBO;
+import lk.ijse.dto.BookDTO;
+import lk.ijse.dto.tm.BookTM;
 import lk.ijse.pageController.PageControl;
 
-public class BookFormController {
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class BookFormController implements Initializable {
     @FXML
     private JFXButton btnClose;
 
     @FXML
-    private TableView<?> bookTable;
+    private TableView<BookTM> bookTable;
 
     @FXML
     private TableColumn<?, ?> colBookId;
+
+    @FXML
+    private TableColumn<?, ?> colName;
 
     @FXML
     private TableColumn<?, ?> colAuthor;
@@ -28,6 +43,10 @@ public class BookFormController {
 
     PageControl pageControl = new PageControl();
 
+    BookBO bookBO = (BookBO) BOFactory.getBoFactory().BOTypes(BOFactory.BOTypes.BOOK);
+
+    ObservableList<BookTM> obList = FXCollections.observableArrayList();
+
     @FXML
     void closeOnAction(ActionEvent event) {
         pageControl.closeWindow(btnClose);
@@ -36,5 +55,37 @@ public class BookFormController {
     @FXML
     void newBookOnAction(ActionEvent event) {
 
+    }
+
+    public void loadAllBooks(){
+        try {
+            List<BookDTO> bookDTOS = bookBO.getAllBook();
+            for (BookDTO bookDTO : bookDTOS){
+                obList.add(new BookTM(
+                    bookDTO.getId(),
+                    bookDTO.getTitle(),
+                    bookDTO.getAuthor(),
+                    bookDTO.getGenre(),
+                    bookDTO.getAvailabilityStatus()
+                ));
+            }
+            bookTable.setItems(obList);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void setCellValueFactory() {
+        colBookId.setCellValueFactory(new PropertyValueFactory<>("BookId"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("Name"));
+        colAuthor.setCellValueFactory(new PropertyValueFactory<>("Author"));
+        colGenre.setCellValueFactory(new PropertyValueFactory<>("Genre"));
+        colAvailability_Status.setCellValueFactory(new PropertyValueFactory<>("AvailabilityStatus"));
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        loadAllBooks();
+        setCellValueFactory();
     }
 }
