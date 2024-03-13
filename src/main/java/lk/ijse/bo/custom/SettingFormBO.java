17d8014a5c9c2f89159f179c6ecfb61fbd574470
LRLayan <rameshlayan4@gmail.com>
2024-03-14 01:10:46 +0530

    getAllAdmin() updateAdminUsername() updateUserUsername() updateUserPassword() updateAdminPassword()

--- src/main/java/lk/ijse/bo/custom/SettingFormBO.java
+++ src/main/java/lk/ijse/bo/custom/SettingFormBO.java
@@ -1,11 +1,18 @@
 package lk.ijse.bo.custom;
 
 import lk.ijse.bo.SuperBO;
+import lk.ijse.dto.AdminDTO;
 import lk.ijse.dto.RegisterDTO;
+import lk.ijse.dto.UserDTO;
 
 import java.util.List;
 
 public interface SettingFormBO extends SuperBO {
-    boolean updateCredential(RegisterDTO userDTO);
     List<RegisterDTO> getAllUser();
+    List<AdminDTO> getAllAdmin();
+    boolean updateAdminUsername(AdminDTO adminDTO);
+    boolean updateUserUsername(UserDTO userDTO);
+    boolean updateUserPassword(UserDTO userDTO);
+    boolean updateAdminPassword(AdminDTO adminDTO);
+
 }
