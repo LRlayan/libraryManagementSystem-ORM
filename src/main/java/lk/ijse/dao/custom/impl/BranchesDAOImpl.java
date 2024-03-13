45bdd7103a5fb3ce121d08ad779ea9c0cef641d0
LRLayan <rameshlayan4@gmail.com>
2024-03-13 20:20:08 +0530

    update() , delete()

--- src/main/java/lk/ijse/dao/custom/impl/BranchesDAOImpl.java
+++ src/main/java/lk/ijse/dao/custom/impl/BranchesDAOImpl.java
@@ -1,8 +1,11 @@
 package lk.ijse.dao.custom.impl;
 
+import jakarta.persistence.Query;
 import lk.ijse.config.FactoryConfiguration;
 import lk.ijse.dao.custom.BranchesDAO;
+import lk.ijse.entity.Admin;
 import lk.ijse.entity.Branches;
+import lk.ijse.entity.User;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -25,12 +28,42 @@ public class BranchesDAOImpl implements BranchesDAO {
 
     @Override
     public boolean update(Branches branches) {
-        return false;
+       Session session = FactoryConfiguration.getInstance().getSession();
+       Transaction transaction = session.beginTransaction();
+
+        Query query = session.createQuery("UPDATE Branches b SET b.id = :id , b.branchIdNumber = :branchIdNumber , b.branchName = :branchName WHERE b.id = :branchId");
+        query.setParameter("id" , branches.getId());
+        query.setParameter("branchIdNumber" , branches.getBranchIdNumber());
+        query.setParameter("branchName" , branches.getBranchName());
+
+        query.setParameter("branchId" , branches.getId());
+
+        int row = query.executeUpdate();
+
+       transaction.commit();
+       session.close();
+       return row > 0;
     }
 
     @Override
     public boolean delete(long id) {
-        return false;
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+        Branches branches = session.get(Branches.class,id);
+
+        for (User user : branches.getUserList()){
+            session.delete(user);
+        }
+
+        for (Admin admin : branches.getAdminList()){
+            session.delete(admin);
+        }
+        session.delete(branches);
+
+        transaction.commit();
+        session.close();
+        return true;
     }
 
     @Override
