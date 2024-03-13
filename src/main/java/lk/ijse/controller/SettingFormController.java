ae02f3e09ec2348eacbf25c5233dcb89c9ac7920
LRLayan <rameshlayan4@gmail.com>
2024-03-14 01:11:17 +0530

    bugfix

--- src/main/java/lk/ijse/controller/SettingFormController.java
+++ src/main/java/lk/ijse/controller/SettingFormController.java
@@ -7,9 +7,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import lk.ijse.bo.BOFactory;
-import lk.ijse.bo.custom.RegisterFormBO;
 import lk.ijse.bo.custom.SettingFormBO;
+import lk.ijse.dto.AdminDTO;
 import lk.ijse.dto.RegisterDTO;
+import lk.ijse.dto.UserDTO;
 import lk.ijse.pageController.PageControl;
 
 import java.util.List;
@@ -70,38 +71,107 @@ public class SettingFormController {
         String newPassword = txtNewPassword.getText();
         String confirmPassword = txtConfirmPassword.getText();
 
-        if (!currentUsername.isEmpty() && !newUsername.isEmpty() && !confirmUsername.isEmpty()){
+        List<AdminDTO> adminDTOList = settingFormBO.getAllAdmin();
 
-             boolean isChecked = checkingUsername(confirmUsername,newUsername,confirmUsername);
-             if (isChecked){
-                 var userDTO = new RegisterDTO();
-                 userDTO.setName(confirmUsername);
-                 boolean isUpdated = settingFormBO.updateCredential(userDTO);
+        for (int i = 0; i < adminDTOList.size(); i++) {
+            if ( txtCurrentUsername.getText().equals(adminDTOList.get(i).getUsername())) {
+                if (!currentUsername.isEmpty() && !newUsername.isEmpty() && !confirmUsername.isEmpty()) {
 
-                 if (isUpdated){
-                     new Alert(Alert.AlertType.INFORMATION,"Updated Successfully").show();
-                 }
-             }
-        }else if (!currentPassword.isEmpty() && !newPassword.isEmpty() && !confirmPassword.isEmpty()){
+                    boolean isChecked = checkingUsername(confirmUsername, newUsername, confirmUsername);
+                    if (isChecked) {
+                        var adminDTO = new AdminDTO();
+                        adminDTO.setUsername(confirmUsername);
+                        boolean isUpdated = settingFormBO.updateAdminUsername(adminDTO);
 
+                        if (isUpdated) {
+                            new Alert(Alert.AlertType.INFORMATION, "Updated Successfully").show();
+                        }
+                    }
+                } else if (!currentPassword.isEmpty() && !newPassword.isEmpty() && !confirmPassword.isEmpty()) {
+                    boolean isChecked = checkingPassword(currentPassword, newPassword, confirmPassword);
+                    if (isChecked) {
+                        var adminDTO = new AdminDTO();
+                        adminDTO.setPassword(confirmPassword);
+                        boolean isUpdated = settingFormBO.updateAdminPassword(adminDTO);
+
+                        if (isUpdated) {
+                            new Alert(Alert.AlertType.INFORMATION, "Updated Successfully").show();
+                        }
+                    }
+                }
+            }else {
+                if (!currentUsername.isEmpty() && !newUsername.isEmpty() && !confirmUsername.isEmpty()) {
+
+                    boolean isChecked = checkingUsername(currentUsername, newUsername, confirmUsername);
+                    if (isChecked) {
+                        var userDTO = new UserDTO();
+                        userDTO.setName(confirmUsername);
+                        boolean isUpdated = settingFormBO.updateUserUsername(userDTO);
+
+                        if (isUpdated) {
+                            new Alert(Alert.AlertType.INFORMATION, "Updated Successfully").show();
+                        }
+                    }
+                } else if (!currentPassword.isEmpty() && !newPassword.isEmpty() && !confirmPassword.isEmpty()) {
+                    boolean isChecked = checkingPassword(currentPassword, newPassword, confirmPassword);
+                    if (isChecked) {
+                        var userDTO = new UserDTO();
+                        userDTO.setPassword(confirmPassword);
+                        boolean isUpdated = settingFormBO.updateUserPassword(userDTO);
+
+                        if (isUpdated) {
+                            new Alert(Alert.AlertType.INFORMATION, "Updated Successfully").show();
+                        }
+                    }
+                }
+            }
         }
+    }
+
+    private boolean checkingPassword(String currentPassword, String newPassword, String confirmPassword) {
+        boolean isChecked = false;
+        List<RegisterDTO> registerUsers = settingFormBO.getAllUser();
+        List<AdminDTO> adminDTOS = settingFormBO.getAllAdmin();
+
+        for (int i = 0; i < registerUsers.size(); i++) {
+            for (int f = 0; f < adminDTOS.size(); f++) {
+                if (currentPassword.equals(registerUsers.get(i).getName()) || currentPassword.equals(adminDTOS.get(f).getUsername())) {
+                    if (newPassword.equals(confirmPassword)) {
+                        isChecked = true;
+                    } else {
+                        lblNewUsername.setText("incorrect username.please reEnter new username.");
+                    }
+                } else {
+                    lblCurrentUsername.setText("Please input correct username");
+                }
+            }
+        }
+        return isChecked;
     }
 
     private boolean checkingUsername(String currentUsername , String newUsername , String confirmUsername) {
         boolean isChecked = false;
         List<RegisterDTO> registerUsers = settingFormBO.getAllUser();
+        List<AdminDTO> adminDTOS = settingFormBO.getAllAdmin();
 
-        for (int i = 0; i < registerUsers.size(); i++){
-            if (currentUsername.equals(registerUsers.get(i).getName())){
-                if (newUsername.equals(confirmUsername)){
-                   isChecked = true;
-                }else {
-                    lblNewUsername.setText("incorrect username.please reEnter new username.");
+        for (int i = 0; i < registerUsers.size(); i++) {
+            for (int f = 0; f < adminDTOS.size(); f++) {
+                if (currentUsername.equals(registerUsers.get(i).getName()) || currentUsername.equals(adminDTOS.get(f).getUsername())) {
+                    if (newUsername.equals(confirmUsername)) {
+                        isChecked = true;
+                    } else {
+                        lblNewUsername.setText("incorrect username.please reEnter new username.");
+                    }
+                } else {
+                    lblCurrentUsername.setText("Please input correct username");
                 }
-            }else {
-                lblCurrentUsername.setText("Please input correct username");
             }
         }
         return isChecked;
+    }
+
+    @FXML
+    void closeWindowOnAction(ActionEvent event) {
+        pageControl.closeWindow(btnClose);
     }
 }
