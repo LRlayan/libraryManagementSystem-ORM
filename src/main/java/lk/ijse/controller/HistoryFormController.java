6b9fa52c8855fbd76fdae1220fd99d82e147b16c
LRLayan <rameshlayan4@gmail.com>
2024-03-14 19:51:22 +0530

    closeWindowOnAction()

--- src/main/java/lk/ijse/controller/HistoryFormController.java
+++ src/main/java/lk/ijse/controller/HistoryFormController.java
@@ -1,4 +1,45 @@
 package lk.ijse.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import lk.ijse.pageController.PageControl;
+
 public class HistoryFormController {
+    @FXML
+    private JFXButton btnClose;
+
+    @FXML
+    private TableView<?> historyTable;
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colUserName;
+
+    @FXML
+    private TableColumn<?, ?> colBookName;
+
+    @FXML
+    private TableColumn<?, ?> colTime;
+
+    @FXML
+    private TableColumn<?, ?> colStartDate;
+
+    @FXML
+    private TableColumn<?, ?> colReturnDate;
+
+    @FXML
+    private TableColumn<?, ?> colReturnBook;
+
+
+    PageControl pageControl = new PageControl();
+
+    @FXML
+    void closeWindowOnAction(ActionEvent event) {
+        pageControl.closeWindow(btnClose);
+    }
 }
