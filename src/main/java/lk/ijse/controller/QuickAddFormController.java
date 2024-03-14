82a5b705e2fd0a6d23d91a6877a02530011a7334
LRLayan <rameshlayan4@gmail.com>
2024-03-14 19:52:10 +0530

    closeWindowOnAction() quickAddOnAction() setBookTitle()

--- src/main/java/lk/ijse/controller/QuickAddFormController.java
+++ src/main/java/lk/ijse/controller/QuickAddFormController.java
@@ -6,8 +6,14 @@ import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.controls.JFXTimePicker;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import lk.ijse.pageController.PageControl;
 
-public class QuickAddFormController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class QuickAddFormController implements Initializable {
     @FXML
     private JFXButton btnClose;
 
@@ -26,13 +32,30 @@ public class QuickAddFormController {
     @FXML
     private JFXTimePicker txtTime;
 
+    PageControl pageControl = new PageControl();
+    static String title;
     @FXML
-    void closeWindowOnAction(ActionEvent event) {
-
+    void closeWindowOnAction(ActionEvent event) throws IOException {
+        pageControl.closeWindow(btnClose);
     }
 
     @FXML
     void quickAddOnAction(ActionEvent event) {
+        String startDate = String.valueOf(txtStartDate.getValue());
+        String time = String.valueOf(txtTime.getValue());
+        String returnDate = String.valueOf(txtReturnDate.getValue());
+        String name = txtName.getText();
+        String title = txtTitle.getText();
 
+
+    }
+
+    private void setBookTitle() {
+       txtTitle.setText(title);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setBookTitle();
     }
 }
