1a955de94ea85743b13333eb7a74393e1b3ba894
LRLayan <rameshlayan4@gmail.com>
2024-03-14 13:50:16 +0530

    getAllBooks()

--- src/main/java/lk/ijse/bo/custom/impl/BookOBJBOImpl.java
+++ src/main/java/lk/ijse/bo/custom/impl/BookOBJBOImpl.java
@@ -1,6 +1,26 @@
 package lk.ijse.bo.custom.impl;
 
 import lk.ijse.bo.custom.BookOBJBO;
+import lk.ijse.dao.DAOFactory;
+import lk.ijse.dao.custom.BookDAO;
+import lk.ijse.dto.BookDTO;
+import lk.ijse.entity.Books;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BookOBJBOImpl implements BookOBJBO {
+
+  BookDAO bookDAO = (BookDAO) DAOFactory.getDaoFactory().DAOTypes(DAOFactory.DAOTypes.BOOK);
+
+    @Override
+    public List<BookDTO> getAllBooks() {
+        List<Books> books = bookDAO.getAll();
+        List<BookDTO> bookDTOS = new ArrayList<>();
+
+        for (Books allBook : books){
+            bookDTOS.add(new BookDTO(allBook.getId(), allBook.getTitle(), allBook.getGenre(), allBook.getAuthor(), allBook.getAvailabilityStatus(), allBook.getImage()));
+        }
+        return bookDTOS;
+    }
 }
