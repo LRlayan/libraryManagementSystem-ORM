e543cf6e565f0ce0ac81bbd92a6b51887b429278
LRLayan <rameshlayan4@gmail.com>
2024-03-15 19:56:14 +0530

    updateUsername() updatePassword()

--- src/main/java/lk/ijse/dao/custom/UserDAO.java
+++ src/main/java/lk/ijse/dao/custom/UserDAO.java
@@ -4,4 +4,6 @@ import lk.ijse.dao.CrudDAO;
 import lk.ijse.entity.User;
 
 public interface UserDAO extends CrudDAO<User> {
+    boolean updateUsername(String confirmUsername, long id);
+    boolean updatePassword(String confirmPassword, long id);
 }
