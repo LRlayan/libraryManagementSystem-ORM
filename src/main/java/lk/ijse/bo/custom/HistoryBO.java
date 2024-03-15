46cb8ee369521e3b9fb5ecb54d683c9fad05cbb6
LRLayan <rameshlayan4@gmail.com>
2024-03-15 18:00:46 +0530

    getAllTransaction()  updateBookStatus()

--- src/main/java/lk/ijse/bo/custom/HistoryBO.java
+++ src/main/java/lk/ijse/bo/custom/HistoryBO.java
@@ -1,6 +1,12 @@
 package lk.ijse.bo.custom;
 
 import lk.ijse.bo.SuperBO;
+import lk.ijse.dto.HistoryDTO;
+
+import java.util.List;
 
 public interface HistoryBO extends SuperBO {
+    List<HistoryDTO> getAllTransaction();
+
+    boolean updateBookStatus(HistoryDTO historyDTO);
 }
